8d2b8b76ebc6136dba85acde974bb3ba347f7d26
StombieIT <stombie@yandex.ru>
2023-02-25 18:05:56 +0300

    consider dev and prod modes

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,26 @@ const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
-module.exports = {
-    mode: "development",
-    entry: {
-        index: "./src/index.js"
-    },
-    output: {
-        filename: "[name]__[contenthash:8].bundle.js",
-        path: path.resolve(__dirname, "dist")
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./public/index.html",
-            minify: {
-                collapseWhitespace: true
-            }
-        }),
-        new CleanWebpackPlugin()
-    ],
+module.exports = function(env) {
+    const isDevelopment = env.NODE_ENV === "dev"
+
+    return {
+        mode: isDevelopment ? "development" : "production",
+        entry: {
+            index: "./src/index.js"
+        },
+        output: {
+            filename: "[name]__[contenthash:8].bundle.js",
+            path: path.resolve(__dirname, "dist")
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./public/index.html",
+                minify: {
+                    collapseWhitespace: !isDevelopment
+                },
+            }),
+            new CleanWebpackPlugin()
+        ],
+    }
 }
