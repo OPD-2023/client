f12e79257fb515aaf9c7aa30004e40c06403d439
StombieIT <stombie@yandex.ru>
2023-02-25 19:52:30 +0300

    add react support

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client"
+
+const root = createRoot(
+    document.getElementById("root") as HTMLElement
+)
+
+root.render(
+    <div>ficko</div>
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { ProvidePlugin } = require("webpack")
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 const babelRule = (test, ...additionalPresets) => ({
@@ -15,31 +16,36 @@ const babelRule = (test, ...additionalPresets) => ({
     ]
 })
 
-module.exports = env => {
-    const isDevelopment = env.NODE_ENV === "dev"
-
-    return {
-        mode: isDevelopment ? "development" : "production",
-        entry: {
-            index: ["@babel/polyfill", "./src/index.ts"]
-        },
-        output: {
-            filename: "[name]__[contenthash:8].bundle.js",
-            path: path.resolve(__dirname, "dist")
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: "./public/index.html",
-                minify: {
-                    collapseWhitespace: !isDevelopment
-                },
-            }),
-            new CleanWebpackPlugin()
-        ],
-        module: {
-            rules: [
-                babelRule(/\.ts$/, "@babel/preset-typescript")
-            ]
-        }
+module.exports = {
+    mode: "development",
+    entry: {
+        index:  ["@babel/polyfill", "./src/index.tsx"]
+    },
+    output: {
+        filename: "[name]__[contenthash:8].bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+            minify: {
+                collapseWhitespace: true
+            },
+        }),
+        new ProvidePlugin({
+            React: "react"
+        }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            babelRule(/\.js$/),
+            babelRule(/\.jsx$/, "@babel/preset-react"),
+            babelRule(/\.ts$/, "@babel/preset-typescript"),
+            babelRule(/\.tsx$/, "@babel/preset-react", "@babel/preset-typescript")
+        ]
+    },
+    devServer: {
+        port: 1337
     }
 }
