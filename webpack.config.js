1ef99353353743e44cd3e81486f1c8c5baab653d
StombieIT <stombie@yandex.ru>
2023-02-26 18:42:48 +0300

    add style suport

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 const { ProvidePlugin } = require("webpack")
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
+const NODE_MODULES_REGEXP = /node_modules/
+
 const babelRule = (test, ...additionalPresets) => ({
     test,
-    exclude: /node_modules/,
+    exclude: NODE_MODULES_REGEXP,
     use: [
         {
             loader: "babel-loader",
@@ -16,8 +18,35 @@ const babelRule = (test, ...additionalPresets) => ({
     ]
 })
 
+const styleRule = (test, useCallback) => {
+    let use = [
+        "style-loader",
+        {
+            loader: "css-loader",
+            options: {
+                modules: {
+                    localIdentName: "[folder]__[local]--[hash:base64:8]"
+                }
+            }
+        }
+    ]
+    
+    if (useCallback) {
+        use = useCallback(use)
+    }
+
+    return {
+        test,
+        exclude: NODE_MODULES_REGEXP,
+        use
+    }
+}
+
 module.exports = {
     mode: "development",
+    resolve: {
+        extensions: [".js", ".ts", ".jsx", ".tsx"]
+    },
     entry: {
         index:  ["@babel/polyfill", "./src/index.tsx"]
     },
@@ -42,10 +71,17 @@ module.exports = {
             babelRule(/\.js$/),
             babelRule(/\.jsx$/, "@babel/preset-react"),
             babelRule(/\.ts$/, "@babel/preset-typescript"),
-            babelRule(/\.tsx$/, "@babel/preset-react", "@babel/preset-typescript")
+            babelRule(/\.tsx$/, "@babel/preset-react", "@babel/preset-typescript"),
+            styleRule(/\.css$/),
+            styleRule(/\.styl$/, use => [...use, "stylus-loader"]),
+            {
+                test: /\.(png|jpg|jpeg|svg)$/,
+                use: "file-loader"
+            }
         ]
     },
     devServer: {
-        port: 1337
+        port: 1337,
+        hot: true
     }
 }
