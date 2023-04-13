ba8a5db2e61fdb068d93c3e19f4a8cba3d58b0cc
StombieIT <stombie@yandex.ru>
2023-04-13 17:50:31 +0300

    Созданы основные элементы бизнес-логики, добавлена частичная выгрузка данных в localStorage

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,23 @@
 import { createRoot } from "react-dom/client"
+import { configure } from "mobx"
+
+import RootStoreProvider from "@services/components/RootStoreProvider"
 import App from "@components/App/App"
+
+/**
+ *
+ * Разрешаем изменять state напрямую, без создания actions
+ */
+configure({
+    enforceActions: "never"
+})
 
 const root = createRoot(
     document.getElementById("root") as HTMLElement
 )
 
 root.render(
-    <App />
+    <RootStoreProvider>
+        <App />
+    </RootStoreProvider>
 )
