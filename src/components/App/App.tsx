ddc4b5627459a43610762f7fff02790691d24d8a
Vladislav Yartsev <vladislav.yartsev@vk.team>
2023-04-15 16:35:54 +0300

    Удалены неиспользуемые файлы

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,9 @@
 import {FC} from "react"
 import {observer} from "mobx-react"
-import classNames from "classnames"
 
 import ProductsSearch from "@components/ProductsSearch/ProductsSearch"
-import useRootStore from "@services/hooks/useRootStore"
-
-import classes from "./App.module.styl"
 
 const App: FC = observer(() => {
-    const { productsSearchStore } = useRootStore()
-
-    const listClass: string = classNames(classes.list, {
-        [classes.__loading]: productsSearchStore.productsAreLoading
-    })
-
     return <main>
         <ProductsSearch />
     </main>
