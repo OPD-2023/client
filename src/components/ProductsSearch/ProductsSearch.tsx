7bdbfef5a52249b3d237394d890ac98c422c9fb7
Vladislav Yartsev <vladislav.yartsev@vk.team>
2023-04-15 14:24:56 +0300

    Небольшие фиксы

--- src/components/ProductsSearch/ProductsSearch.tsx
+++ src/components/ProductsSearch/ProductsSearch.tsx
@@ -2,7 +2,6 @@ import {FC} from "react"
 import {observer} from "mobx-react"
 import classNames from "classnames"
 
-import Container from "@components/Container/Container"
 import Search24 from "@components/Search24"
 import useRootStore from "@services/hooks/useRootStore"
 
@@ -11,32 +10,30 @@ import classes from "./ProductsSearch.module.styl"
 const ProductsSearch: FC = observer(() => {
     const { productsSearchStore } = useRootStore()
 
-    const isProductsExist: boolean = !!productsSearchStore.products && !!productsSearchStore.products.length
+    const areProductsExist: boolean = !!productsSearchStore.products && !!productsSearchStore.products.length
 
     const dropdownClass: string = classNames(classes.dropdown, {
         [classes.__loading]: productsSearchStore.productsAreLoading
     })
 
-    return <Container>
-        <div className={classes.wrapper}>
-            <input className={classes.field}
-                   value={productsSearchStore.field}
-                   onChange={evt => productsSearchStore.field = evt.target.value} />
-            <button className={classes.button}>
-                Найти
-            </button>
-            {
-                isProductsExist && <ul className={dropdownClass}>
-                    {
-                        productsSearchStore.products!.map(product => <li className={classes.item} key={product.article}>
-                            <Search24 />
-                            <span className={classes.payload}>{product.title}</span>
-                        </li>)
-                    }
-                </ul>
-            }
-        </div>
-    </Container>
+    return <div className={classes.wrapper}>
+        <input className={classes.field}
+               value={productsSearchStore.field}
+               onChange={evt => productsSearchStore.field = evt.target.value} />
+        <button className={classes.button}>
+            Найти
+        </button>
+        {
+            areProductsExist && <ul className={dropdownClass}>
+                {
+                    productsSearchStore.products!.map(product => <li className={classes.item} key={product.article}>
+                        <Search24 />
+                        <span className={classes.payload}>{product.title}</span>
+                    </li>)
+                }
+            </ul>
+        }
+    </div>
 })
 
 export default ProductsSearch
