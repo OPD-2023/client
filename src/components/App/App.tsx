3aacae4ee03caf0b5757cb0b208d048d3d1d69c6
Bednyakov <bednyakov99@gmail.com>
2023-04-24 09:24:27 +0300

    Добавил компонент карточки товара

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,13 +4,24 @@ import {observer} from "mobx-react"
 import ProductsSearch from "@components/ProductsSearch/ProductsSearch"
 import Container from "@components/Container/Container"
 import GroupsCatalog from "@components/GroupsCatalog/GroupsCatalog";
+import ProductCard from "@components/ProductCard/ProductCard";
 
 import classes from "./App.module.styl"
+
+const product = {
+    partNumber: 'Артикул: 302-4SS',
+    title: 'ЦЕНТРАТОР НАРУЖНЫЙ С ГИДРОДОМКРАТОМ',
+    price: 182300,
+    currency: "руб",
+    image: "https://static.overlay-tech.com/assets/7016dd7e-66d0-43a1-9870-b95fec2e2d24.png",
+    rating: 4.5,
+};
 
 const App: FC = observer(() => {
     return <Container className={classes.application}>
         <GroupsCatalog />
         <ProductsSearch />
+        <ProductCard product={ product } />
     </Container>
 })
 
